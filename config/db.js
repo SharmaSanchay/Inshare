12f32ddbffe595f6a1d40cc1f0e65a132950e0c2
Sanchay Sharma <sanchaysharm18@gmail.com>
2024-10-05 16:17:11 +0530

    Remove specific files

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const mongoose = require("mongoose");
-function connectDB(){
+function connectDB() {
     mongoose.connect(process.env.MONGO_CONNECTION_URL).then(() => {
         console.log("Database connected");
     }).catch(err => {
--- routes/download.js
+++ routes/download.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const File = require('../models/file');
-router.get('/:uuid',async (req,res)=>{
-    const file = await File.findOne({uuid:req.params.uuid});
-    if(!file){
-        return res.render('download',{error:"Link has been expired"});
-    }  
+router.get('/:uuid', async (req, res) => {
+    const file = await File.findOne({ uuid: req.params.uuid });
+    if (!file) {
+        return res.render('download', { error: "Link has been expired" });
+    }
     const filePath = `${__dirname}/../${file.path}`;
     res.download(filePath);
 })
